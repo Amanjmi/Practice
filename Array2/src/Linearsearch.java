182ee6423e49c5609fa8c85f1d808cb6c395dad6
Aman <amanurrehman68@gmail.com>
2022-11-15 09:17:09 +0530

    Adding Array2 folder

---
+++ Array2/src/Linearsearch.java
@@ -0,0 +1,32 @@
+//Searching of an element in the given array
+import java.util.Scanner;
+
+public class Linearsearch{
+
+	public static void main(String[] args) {
+		int[] ar= {10,20,40,30,60,70,80,90};
+		boolean flag=false;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the key to be searched");
+		int key=scan.nextInt();
+		
+		for(int i=0;i<ar.length;i++)
+		{
+			if(key==ar[i])
+			{
+				System.out.println("Key "+key+" found at index " + i);
+				flag=true;
+				break;
+			}
+		}
+		
+		if(flag==false)
+		{
+		System.out.println("Key not found");
+		}
+
+	
+
+	}
+
+}
